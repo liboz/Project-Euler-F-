0687cb9dfaefa503da2a21dd1546a26e337fe6a4
Libo Zeng <libo.zeng@boeing.com>
2016-01-27 16:09:19 -0500

    Problem 2

---
+++ Problem 2/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    let rec fibonacci x = 
+        if x = 0 then
+            1
+        elif x = 1 then
+            1
+        else
+            fibonacci (x-1) + fibonacci (x-2)
+    
+    let value = [1..32]
+                |> List.map (fun x -> fibonacci x)
+                |> List.filter (fun x -> x % 2 = 0)
+                |> List.sum
+    
+    printfn "The value is %d." value
+    0 // return an integer exit code
