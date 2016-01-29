0d0919a4a2ac18b2da0c73229b09e98ab0688e2f
Libo Zeng <libo.zeng@boeing.com>
2016-01-29 09:02:43 -0500

    Problem 6

---
+++ Problem 6/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System.Diagnostics
+
+
+type PrimeFactor = {number: int; count: int}
+
+[<EntryPoint>]
+let main argv = 
+    let stopWatch = Stopwatch.StartNew()
+    
+    let baseArray = [|1..100|]
+
+    let sumofSquares x = x 
+                        |> Array.map (fun i -> i*i)
+                        |> Array.sum
+
+    let squareofSum x =  pown (Array.sum x) 2
+
+    let value = (squareofSum baseArray) - (sumofSquares baseArray)
+
+    stopWatch.Stop()
+    printfn "The value is %d and it took %f" value stopWatch.Elapsed.TotalMilliseconds //About 0.9ms 
+    0 // return an integer exit code
