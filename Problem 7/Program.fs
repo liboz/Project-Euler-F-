40c6be07df294d01de778969f87050406b597eed
Libo Zeng <libo.zeng@boeing.com>
2016-01-29 15:47:52 -0500

    Problem 7/8/9

---
+++ Problem 7/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System.Diagnostics
+
+
+type PrimeFactor = {number: int; count: int}
+
+[<EntryPoint>]
+let main argv = 
+    let stopWatch = Stopwatch.StartNew()
+    
+    let maxSearchPrime x = int (System.Math.Round (sqrt (float x)))
+    
+    let isPrime (x) = not ({3..(maxSearchPrime x)} |> Seq.exists (fun i -> x % i = 0))
+
+    let value = Seq.unfold (fun x -> Some(x, x + 2)) 3
+                |> Seq.filter (fun i -> isPrime i)
+                |> Seq.item(9999) //This is 10000th item instead of 10001 because we skip the number 2. Note 0-index
+
+    stopWatch.Stop()
+    printfn "The value is %d and it took %f" value stopWatch.Elapsed.TotalMilliseconds //About 220ms 
+    0 // return an integer exit code
---
+++ Problem 9/Program.fs
@@ -0,0 +1,29 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System.Diagnostics
+
+[<EntryPoint>]
+let main argv = 
+    
+    let stopWatch = Stopwatch.StartNew()
+
+    let isPythagoreanTriple a b c = (a*a + b*b = c*c) = true
+    let findPossibleTripletPairs i array givenConstraint = array|> Array.filter (fun j -> j > i) 
+                                                                |> Array.map (fun j -> (j, givenConstraint - j - i))
+                                                                |> Array.filter (fun (j, k) -> j < k && isPythagoreanTriple i j k)
+
+    let baseArray = [|1..1000|]
+    let additionConstraint = 1000
+
+    let value = baseArray
+                |> Array.map (fun i -> (i, findPossibleTripletPairs i baseArray additionConstraint))
+                |> Array.filter (fun (i, jk) -> (jk.Length <> 0))
+                |> Array.map (fun (i, jk) -> 
+                    (i, fst jk.[0], snd jk.[0]))
+
+    let val1, val2, val3 = value.[0]
+
+    stopWatch.Stop()
+    printfn "The values are %d, %d, %d and it took %f" val1 val2 val3 stopWatch.Elapsed.TotalMilliseconds //About 25ms 
+
+    0 // return an integer exit code
