b6e2281cdd90ba990836787160fbc003fe50a1b6
Libo Zeng <libo.zeng@boeing.com>
2016-02-01 13:36:24 -0500

    Problem 10/461

---
+++ Problem 10/Program.fs
@@ -0,0 +1,47 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System.Diagnostics
+open System.Collections.Generic
+open System
+
+[<EntryPoint>]
+let main argv = 
+    let max = 2000000L
+
+    let stopWatch = Stopwatch.StartNew()
+    //Using Recursion
+    let rec sieve list = 
+        match list with 
+        | head::tail  when head*head < max ->  head :: (sieve  <|  List.except [head*head..head..max] tail)
+        | head::tail -> head::tail
+        | [] -> []
+    
+    let value = 
+        let allbut2 = sieve [3L..2L..max] 
+                            |> List.sum
+        allbut2 + 2L
+
+    stopWatch.Stop()
+    printfn "The values are %d and it took %f" value stopWatch.Elapsed.TotalMilliseconds //About 10 seconds 
+    
+
+    stopWatch.Reset()
+    stopWatch.Start() 
+    //Using Dictionary
+    let sieve (n:int64) =
+        let primes = new Dictionary<int64, bool>()
+        for i in 3L..2L..n do primes.Add(i, true)
+        for i in 3L..2L..int64(sqrt(double(n))) do //We only need to go up to the sqrt of the max
+            if primes.[i] = true then
+                for j in i*i..i..n do //Start at i squared to do the sieve properly!
+                    primes.[j] <- false //Set non-primes to false
+        let value = seq { for i in 3L..2L..n do
+                            if primes.[i] = true then
+                                yield i }
+                        |> Seq.sum
+        value + 2L
+    let testvalue2 = sieve max
+    stopWatch.Stop()
+    printfn "The values are %d and it took %f" testvalue2 stopWatch.Elapsed.TotalMilliseconds //About 700ms 
+    
+    0 // return an integer exit code
---
+++ Problem 461/Program.fs
@@ -0,0 +1,57 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System.Diagnostics
+open System.Collections.Generic
+open System
+
+[<EntryPoint>]
+let main argv = 
+    let stopWatch = Stopwatch.StartNew()
+    
+    let f n k = exp (double(k)/double(n)) - 1.0
+
+    let n = 10000
+    let maxLimit = int(Math.Round(float(n)*1.5))
+
+    let FunctionValues = [|1..maxLimit|] |> Array.map (fun k -> f n k)
+
+    let sums = FunctionValues |> Array.map (fun i -> Array.map (fun j -> j + i) FunctionValues) |> Array.concat |> Array.sort
+
+    let mutable (best:double) = 1.0
+    let mutable bestI  = 0
+    let mutable bestMid = 0
+
+    for i in 1..(sums.Length - 1) do
+        let mutable min = 0
+        let mutable max = sums.Length
+        let mutable mid = 0
+        while (max >= min ) do 
+            mid <- (min + max)/2
+            if sums.[i] + sums.[mid] <= Math.PI then
+                min <- mid + 1
+            else
+                max <- mid - 1
+        let error = abs(sums.[i] + sums.[mid] - 3.14159265358979323846)
+        if error < best then
+            best <- error
+            bestI <- i
+            bestMid <- mid
+    
+    let mutable a = 0
+    let mutable b = 0
+    let mutable c = 0
+    let mutable d = 0
+
+    for i in 1..maxLimit do
+        for j in i..maxLimit do
+            if sums.[bestI] = f n i + f n j then
+                a <- i
+                b <- j
+            if sums.[bestMid] = f n i + f n j then
+                c <- i
+                d <- j
+
+    stopWatch.Stop()
+    printfn "The values are %g %d %d %d %d and it took %f" best a b c d  stopWatch.Elapsed.TotalMilliseconds //About 700ms 
+    
+    0 // return an integer exit code
